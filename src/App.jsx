6acf346b33a392e59bcfca25038e80049792ccc7
Daniel Cipriano <danielciprianomussenoho@gmail.com>
2024-11-22 02:39:23 +0000

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./App.css";
+
+function App({ noOfStars = 10 }) {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    function handleClick(getCurrentIndex) {
+        setRating(getCurrentIndex)
+    }
+
+    function handleMouseEnter(getCurrentIndex) {
+        setHover(getCurrentIndex)
+    }
+
+    function handleMouseLeave() {
+       setHover(rating)
+    }
+
+    return (
+        <div className="star-rating">
+        <p>eu sou dado</p>
+            {[...Array(noOfStars)].map((_, index) => {
+                index += 1;
+                return (
+                    <FaStar
+                        key={index}
+                        className={index <= (hover || rating) ? "active" : "inactive"}
+                        onClick={() => handleClick(index)}
+                        onMouseMove={() => handleMouseEnter(index)}
+                        onMouseLeave={() => handleMouseLeave()}
+                        size={40}
+                    />
+                );
+            })}
+            <p>Daniel Cipriano</p>
+        </div>
+    );
+}
+
+export default App;
